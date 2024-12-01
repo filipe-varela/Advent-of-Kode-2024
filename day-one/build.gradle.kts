7f562255a07b3ac666c96bcede79e2705d03d9cd
filipe-varela <bfagdsv250@gmail.com>
2024-12-01 22:56:48 +0000

    Starts day one

---
+++ day-one/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "com.aok.vilp"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
